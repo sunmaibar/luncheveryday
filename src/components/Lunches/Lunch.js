c798b7974f1d044d9812315d78aebdfaefdabb27
Jimmy <jajalaba@yahoo.com.tw>
2020-06-14 06:07:53 +0800

    262_330

--- src/components/Lunches/Lunch.js
+++ src/components/Lunches/Lunch.js
@@ -1,6 +1,11 @@
 import React from "react"
+import Image from "gatsby-image"
+import styles from "../../css/tour.module.css"
+import { FaMap } from "react-icons"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-const Lunch = () => {
+const Lunch = ({ lunch }) => {
+  const { title, cost, slug, road, seatsNum, images } = lunch
   return <div>this is a single Lunch</div>
 }
 
