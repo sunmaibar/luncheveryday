89db65e3d77f30c1cded3d891ab610122134d374
Jimmy <jajalaba@yahoo.com.tw>
2020-06-14 07:27:37 +0800

    1230

--- src/components/Lunches/LunchList.js
+++ src/components/Lunches/LunchList.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react"
+import styles from "../../css/items.module.css"
+import Lunch from "./Lunch"
+import Title from "../Title"
 
 export default class LunchList extends Component {
   state = {
@@ -8,11 +11,15 @@ export default class LunchList extends Component {
 
   componentDidMount() {
     this.setState({
-      lunches: this.props.lunches.edges,
-      sortedLunches: this.props.lunches.edges,
+      lunches: this.props.allLunches.edges,
+      sortedLunches: this.props.allLunches.edges,
     })
   }
   render() {
-    return <div>hello from LUNCHLIST</div>
+    return (
+      <section className={styles.tours}>
+        <Title title="所有" subtitle="餐點" />
+      </section>
+    )
   }
 }
