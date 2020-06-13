aa4b1182f9e313524438f427d23338907e3ff940
Jimmy <jajalaba@yahoo.com.tw>
2020-06-14 05:40:50 +0800

    1200

--- src/components/Home/FeaturedLunches.js
+++ src/components/Home/FeaturedLunches.js
@@ -5,7 +5,31 @@ import Title from "../Title"
 import styles from "../../css/items.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
+const getLunches = graphql`
+  query {
+    allLunches: allContentfulLuncheveryday2020 {
+      edges {
+        node {
+          title
+          cost
+          slug
+          road
+          seatsNum
+          contentful_id
+          images {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const FeaturedLunches = () => {
+  const response = useStaticQuery(getLunches)
+  console.log(response)
   return (
     <section className={styles.tours}>
       <Title title="本週" subtitle="主打" />
