bad99d74f9ef184fbc723ef186c971e4b862c005
Jimmy <jajalaba@yahoo.com.tw>
2020-06-14 05:31:21 +0800

    2

--- src/components/Home/FeaturedLunches.js
+++ src/components/Home/FeaturedLunches.js
@@ -1,5 +1,9 @@
 import React from "react"
 import Lunch from "../Lunches/Lunch"
+import { useStaticQuery, graphql } from "gatsby"
+import Title from "../Title"
+import styles from "../../css/items.module.css"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 const FeaturedLunches = () => {
   return (
