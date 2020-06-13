efd68067b6951c50f62700c236e2789ff478e99d
Jimmy <jajalaba@yahoo.com.tw>
2020-06-14 07:19:46 +0800

    263_1100

--- src/components/Lunches/LunchList.js
+++ src/components/Lunches/LunchList.js
@@ -5,6 +5,13 @@ export default class LunchList extends Component {
     lunches: [],
     sortedLunches: [],
   }
+
+  componentDidMount() {
+    this.setState({
+      lunches: this.props.lunches.edges,
+      sortedLunches: this.props.lunches.edges,
+    })
+  }
   render() {
     return <div>hello from LUNCHLIST</div>
   }
