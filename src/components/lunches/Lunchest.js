95e89831b81e525c4223332073329d2dff347a46
Jimmy <jajalaba@yahoo.com.tw>
2020-06-13 23:48:57 +0800

    retry

--- src/components/lunches/Lunchest.js
+++ src/components/lunches/Lunchest.js
@@ -1,7 +1,36 @@
 import React from "react"
+import LunchList from "./LunchList"
+import { useStaticQuery, graphql } from "gatsby"
+
+const getalllunches = graphql`
+  {
+    allLunches: allContentfulLuncheveryday2020 {
+      edges {
+        node {
+          title
+          cost
+          slug
+          road
+          seatsNum
+          contentful_id
+          images {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Lunchest = () => {
-  return <div>hello from Lunchest</div>
+  return (
+    <div>
+      hello from Lunchest
+      <LunchList />
+    </div>
+  )
 }
 
 export default Lunchest
