dbf1e1b9f60416b00fe1650ed5c45d70adcf8969
Jimmy <jajalaba@yahoo.com.tw>
2020-06-15 21:42:53 +0800

    新增social_link

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -3,10 +3,11 @@ import { FaFacebook, FaInstagram } from "react-icons/fa"
 export default [
   {
     icon: <FaFacebook />,
-    url: "https://twitter.com/?lang=en",
+    url:
+      "https://www.facebook.com/%E4%BF%A1%E7%BE%A9%E5%8D%80%E4%B8%AD%E5%8D%88%E5%90%83%E4%BB%80%E9%BA%BC-107762847645042/",
   },
   {
     icon: <FaInstagram />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://www.instagram.com/luncheveryday2020/",
   },
 ]
