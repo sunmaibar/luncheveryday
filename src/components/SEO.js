5214610f6130b3cffe8343757c6aa0260922cd78
Jimmy <jajalaba@yahoo.com.tw>
2020-06-16 11:25:39 +0800

    up

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -40,7 +40,7 @@ const SEO = ({ title, description }) => {
       <meta property="og:type" content="website" />
       <meta property="og:title" content={siteTitle} />
       <meta property="og:description" content={siteDesc} />
-      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image" content={`${image}${siteUrl}`} />
       <meta property="og:image:width" content="400" />
       <meta property="og:image:height" content="300" />
       {/* twitter Card */}
