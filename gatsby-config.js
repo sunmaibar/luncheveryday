214b76fb204a21510924ea9fb51320dc149ec0b4
Jimmy <jajalaba@yahoo.com.tw>
2020-06-16 11:18:23 +0800

    cc

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,12 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "中午吃什麼",
+    title: "信義區中午吃什麼",
     description: "信義區內的巷弄美食，好吃的午餐都在這裡，別再問中午要吃什麼了",
     author: "@信義區大仁哥aka中二大叔",
     keywords: "信義區,中午吃什麼,巷弄美食,在地推薦,下午茶,餅乾",
     twitterUsername: "@jajalaba",
-    image: "/cover_reCapture.jpg",
+    image: "./cover_reCapture.jpg",
     siteUrl: "https://www.luncheveryday.com",
   },
   /* Your site config here */
